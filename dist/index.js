b1fe03e61ae1c7bef5ad7c784f3952b53219a91d
Maksim20000 <max20088888@mail.ru>
2023-06-02 16:51:47 +1000

    3-lesson-get-query-prams-and-url

--- dist/index.js
+++ dist/index.js
@@ -7,8 +7,40 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
 app.get('/', (req, res) => {
-    let hellowWorld = 'Hellow world!!';
+    let hellowWorld = 'Начальный экран';
     res.send(hellowWorld);
+});
+const products = [{ id: 1, title: 'tomato' }, { id: 2, title: 'potato' }];
+const address = [{ id: 1, value: "Moskov" }, { id: 2, value: 'Vladivostok' }];
+app.get('/products', (req, res) => {
+    if (req.query.title) {
+        let searchString = req.query.title;
+        res.send(products.filter(p => p.title.indexOf(searchString.toString()) > -1));
+    }
+    else {
+        res.send(products);
+    }
+    res.send(products);
+});
+app.get('/products/:id', (req, res) => {
+    let id = req.params.id;
+    let elementProduct = products.find(p => p.id === +id);
+    if (elementProduct) {
+        res.send(elementProduct);
+    }
+    else {
+        res.send(404);
+    }
+});
+app.get('/address/:idUser', (req, res) => {
+    let UserId = req.params.idUser;
+    let elementAddressId = address.find(a => a.id === +UserId);
+    if (elementAddressId) {
+        res.send(elementAddressId);
+    }
+    else {
+        res.send(404);
+    }
 });
 app.listen(port, () => {
     console.log('Вроде все работает ');
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,49 @@ const app = express()
 const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
-    let hellowWorld = 'Hellow world!!'
+    let hellowWorld = 'Начальный экран'
     res.send(hellowWorld)
+})
+
+const products= [{id: 1, title: 'tomato'}, {id:2, title: 'potato'}]
+const address = [{id: 1, value: "Moskov"}, {id: 2,value: 'Vladivostok'}]
+app.get('/products', (req: Request, res: Response) => {
+    if(req.query.title){
+        let searchString = req.query.title
+        res.send(products.filter( p => p.title.indexOf(searchString.toString()) > -1))
+    }else{
+        res.send(products)
+    }
+    res.send(products)
+})
+
+
+
+
+
+app.get('/products/:id', (req: Request, res: Response) => {
+    let id = req.params.id
+    let elementProduct = products.find( p => p.id === +id)
+
+    if(elementProduct){
+        res.send(elementProduct)
+    }else{
+        res.send(404)
+    }
+})
+
+
+
+
+
+app.get('/address/:idUser', (req: Request, res: Response) => {
+    let UserId = req.params.idUser
+    let elementAddressId = address.find( a => a.id === +UserId)
+    if(elementAddressId){
+        res.send(elementAddressId)
+    }else{
+        res.send(404)
+    }
 })
 
 app.listen(port, () => {
